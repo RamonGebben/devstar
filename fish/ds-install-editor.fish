619918346fcc90a89842120404ce37ee96267c91
Ramon Gebben <ramon@vita-care.eu>
2014-02-03 08:08:25 -0500

    ds-install-editor updated.

--- fish/ds-install-editor.fish
+++ fish/ds-install-editor.fish
@@ -1,6 +1,8 @@
 function ds-install-editor -d "Install DevStar Editor"
     cd /devstar/sites/
     git clone ssh://devstar.ra-ge.net/devstar/sites/editor
+    chmod -R g+w editor
     bundle install
     rake db:migrate
+    rake db:data:load_dir
 end 
