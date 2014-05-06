443f3ac868497aa8a0385b67004a72994f24f6aa
Ramon Gebben <ramon@vita-care.eu>
2014-05-06 07:37:12 -0400

    fixed get-routes to fit production env

--- fish/get-routes.fish
+++ fish/get-routes.fish
@@ -1,6 +1,18 @@
 function get-routes
-  cat /devstar/sites/*/config/nginx.conf > /devstar/sites/routes.txt
-  more /devstar/sites/*/config/nginx.conf
-  crow notice "You can acces this log when you like in /devstar/sites/routes.txt"
+  touch /devstar/routes.md
+  cat /dev/null > /devstar/routes.md
+
+  echo "# Development"  >> /devstar/routes.md
+  echo /devstar/sites/*/config/nginx.conf >> /devstar/routes.md
+
+  echo "# Production" >> /devstar/routes.md
+  echo /devstar/production/*/config/production.conf >> /devstar/routes.md
+  crow notice "Updated routes.md"
+
+  echo "# Development"
+  more /devstar/sites/*/config/nginx.conf 
+  echo "# Production"
+  more /devstar/production/*/config/production.conf 
+  crow notice "You can acces this log when you like in /devstar/routes.md"
 end
 
